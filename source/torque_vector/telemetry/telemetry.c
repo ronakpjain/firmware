9227356d4087455adf9ebf9b4d83564764c0bc3c
irving <irvingw@purdue.edu>
2026-07-03 17:25:54 -0400

    GPS Validity and Faults Integration (#401)

--- source/torque_vector/telemetry/telemetry.c
+++ source/torque_vector/telemetry/telemetry.c
@@ -7,21 +7,24 @@
 
 #include "telemetry.h"
 
+#include "can_library/faults_common.h"
 #include "can_library/generated/TORQUE_VECTOR.h"
-#include "sensors.h"
 #include "common/utils/clamp.h"
+#include "sensors.h"
 
 /**
- * @brief Reports telemetry data at 100HZ rate
+ * @brief Reports telemetry data at 25HZ rate
  * Includes: GPS coordinates, velocity, speed, heading
  */
-static_assert(GPS_COORDINATES_PERIOD_MS == TELEMETRY_100HZ_PERIOD_MS);
-static_assert(GPS_VELOCITY_PERIOD_MS == TELEMETRY_100HZ_PERIOD_MS);
-static_assert(GPS_SPEED_HEADING_PERIOD_MS == TELEMETRY_100HZ_PERIOD_MS);
-void report_telemetry_100hz(void) {
-    CAN_SEND_gps_coordinates(nav_pvt.latitude, nav_pvt.longitude);
-    CAN_SEND_gps_velocity(nav_pvt.velNorth, nav_pvt.velEast);
-    CAN_SEND_gps_speed_heading(nav_pvt.groundSpeed, nav_pvt.headingVehicle);
+static_assert(GPS_COORDINATES_PERIOD_MS == TELEMETRY_25HZ_PERIOD_MS);
+static_assert(GPS_VELOCITY_PERIOD_MS == TELEMETRY_25HZ_PERIOD_MS);
+static_assert(GPS_SPEED_HEADING_PERIOD_MS == TELEMETRY_25HZ_PERIOD_MS);
+void report_telemetry_25hz(void) {
+    if (is_clear(FAULT_ID_GPS_INVALID_FIX) && is_clear(FAULT_ID_GPS_WEAK_FIX)) {
+        CAN_SEND_gps_coordinates(nav_pvt.latitude, nav_pvt.longitude);
+        CAN_SEND_gps_velocity(nav_pvt.velNorth, nav_pvt.velEast);
+        CAN_SEND_gps_speed_heading(nav_pvt.groundSpeed, nav_pvt.headingVehicle);
+    }
 }
 
 /**
@@ -30,13 +33,17 @@ void report_telemetry_100hz(void) {
  */
 static_assert(GPS_TIME_PERIOD_MS == TELEMETRY_1HZ_PERIOD_MS);
 void report_telemetry_1hz(void) {
-    CAN_SEND_gps_time(
-        (uint8_t)(nav_pvt.year - 2000),
-        nav_pvt.month,
-        nav_pvt.day,
-        nav_pvt.hour,
-        nav_pvt.minute,
-        nav_pvt.second,
-        (uint16_t)CLAMP(nav_pvt.nano / 1'000'000, 0, 999)
-    );
+    if (is_clear(FAULT_ID_GPS_INVALID_UTC)) {
+        uint16_t milliseconds = (uint16_t)CLAMP(nav_pvt.nano / 1'000'000, 0, 999);
+
+        CAN_SEND_gps_time(
+            (uint8_t)(nav_pvt.year - 2000),
+            nav_pvt.month,
+            nav_pvt.day,
+            nav_pvt.hour,
+            nav_pvt.minute,
+            nav_pvt.second,
+            milliseconds
+        );
+    }
 }
