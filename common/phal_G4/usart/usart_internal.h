04abb8f9c5de792115747d51d58a064ceb9f68d2
Ronak Jain <ronak@ronakpjain.com>
2026-07-11 18:39:21 -0700

    USART: coordinate public send and receive operations

--- common/phal_G4/usart/usart_internal.h
+++ common/phal_G4/usart/usart_internal.h
@@ -19,22 +19,47 @@ typedef struct {
 
 PHAL_USART_State_t *PHAL_USART_internalStateForInstance(USART_TypeDef *instance);
 
-bool PHAL_USART_internalConfigure(
+bool PHAL_USART_internalValidateConfig(const PHAL_USART_Config_t *config);
+bool PHAL_USART_internalPrepareInstance(
+    const PHAL_USART_Config_t *config,
+    uint32_t *baud_register
+);
+bool PHAL_USART_internalInitializeStateAndDma(
     PHAL_USART_Handle_t *handle,
     const PHAL_USART_Config_t *config
 );
-bool PHAL_USART_internalStartTransmit(
+void PHAL_USART_internalConfigureRegisters(
+    PHAL_USART_Handle_t *handle,
+    const PHAL_USART_Config_t *config,
+    uint32_t baud_register
+);
+
+bool PHAL_USART_internalValidateTransmit(
+    const PHAL_USART_Handle_t *handle,
+    const uint8_t *data,
+    size_t length
+);
+bool PHAL_USART_internalArmTransmit(
     PHAL_USART_Handle_t *handle,
     const uint8_t *data,
     size_t length
 );
-bool PHAL_USART_internalStartReceive(
+
+bool PHAL_USART_internalValidateReceive(
+    const PHAL_USART_Handle_t *handle,
+    const uint8_t *data,
+    size_t length
+);
+bool PHAL_USART_internalArmReceive(
     PHAL_USART_Handle_t *handle,
     uint8_t *data,
     size_t length,
     bool idle
 );
-bool PHAL_USART_internalStopReceive(PHAL_USART_Handle_t *handle);
+
+bool PHAL_USART_internalValidateHandle(const PHAL_USART_Handle_t *handle);
+void PHAL_USART_internalDisableReceiveRequests(PHAL_USART_Handle_t *handle);
+void PHAL_USART_internalCompleteStopReceive(PHAL_USART_Handle_t *handle);
 bool PHAL_USART_internalCompleteTransmit(PHAL_USART_Handle_t *handle, bool success);
 void PHAL_USART_internalHandleIRQ(USART_TypeDef *instance);
 
