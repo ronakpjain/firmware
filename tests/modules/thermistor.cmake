4632827856bf53b057d560b7c1dd33c1fe181b22
Ronak Jain <ronak@ronakpjain.com>
2026-07-17 20:25:33 -0700

    check thermistor conversions

---
+++ tests/modules/thermistor.cmake
@@ -0,0 +1,5 @@
+target_sources(
+    unit_tests PRIVATE
+    "${FIRMWARE_SOURCE_DIR}/source/a_box/thermistor/thermistor.c"
+    "${FIRMWARE_SOURCE_DIR}/source/a_box/thermistor/thermistor_tests.cpp"
+)
